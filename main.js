df45c42fd0ddb4caa682c366b2f56dc9d60aaede
t3t3c <kleyguy@gmail.com>
2022-06-20 14:40:32 +0200

    added operate function

--- main.js
+++ main.js
@@ -13,3 +13,18 @@ const multiply = function (a, b) {
 const divide = function (a, b) {
   return a / b;
 };
+
+const operate = function (a, b, operator) {
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+  }
+};
+
+console.log(operate(1,2, '+'));
