c14dd9f6e6c9de2d315b8f10cc7a046b3e5cafa4
t3t3c <kleyguy@gmail.com>
2022-06-20 12:50:36 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+function isTheSame(str1, str2) {
+  return str1 === str2;
+}
+
+console.log(
+  isTheSame(
+    "git@github.com:t3t3c/calculator.git",
+    "git@github.com:t3t3c/calculator.git"
+  )
+);
